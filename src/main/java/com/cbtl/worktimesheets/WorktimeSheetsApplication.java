ce14875a70cc6fc759d9f707441a212a5a71fa53
Fryk <fryk06@gmail.com>
2020-12-17 13:21:27 +0100

    init

---
+++ src/main/java/com/cbtl/worktimesheets/WorktimeSheetsApplication.java
@@ -0,0 +1,13 @@
+package com.cbtl.worktimesheets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorktimeSheetsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorktimeSheetsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cbtl/worktimesheets/WorktimeSheetsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cbtl.worktimesheets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorktimeSheetsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
